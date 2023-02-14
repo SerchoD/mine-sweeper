9383f7125ea8da2e2c6c9364fb758e3dcfd09965
Sergio Díaz <diaz.sergio@protonmail.com>
2023-02-14 01:29:44 -0300

    Revert "making test for security at mergint to main branch"
    This reverts commit 1f53a3f0296e11fbd5ca0e5df79d2e4e2848c87f.

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -42,7 +42,6 @@ const TopBar = ({
 
 	return (
 		<div className='topbar-container'>
-			<h1>TEST de seguridad</h1>
 			<Timer triggerTimer={triggerTimer} />
 			<div className='topbar-select'>
 				<label htmlFor='rows'>Size</label>
